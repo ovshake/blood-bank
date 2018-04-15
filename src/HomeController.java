be5c4fab99ef8e0e65f2cb27792266c6e280d2ae
ovshake <abhishek16005@iiitd.ac.in>
2018-04-15 22:30:17 +0530

    Added the main function and the fxml files

---
+++ src/HomeController.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class HomeController {
+    public Button loginButton;
+    public Button signUpButton;
+
+    public void onClickLoginButton(ActionEvent actionEvent) {
+        System.out.println("Hello World !");
+    }
+
+    public void onClickSignUp(ActionEvent actionEvent) {
+    }
+}
